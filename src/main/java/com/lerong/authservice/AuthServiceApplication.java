37af731b1e9dc1072532c8fba1c41235821affdc
gong-f <976310867@qq.com>
2026-01-19 23:10:08 +0800

    提交启动类

--- src/main/java/com/lerong/authservice/AuthServiceApplication.java
+++ src/main/java/com/lerong/authservice/AuthServiceApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthServiceApplication {
 
+    // 启动入口
     public static void main(String[] args) {
+
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 
